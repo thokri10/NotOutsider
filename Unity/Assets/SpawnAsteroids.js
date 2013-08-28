39f635217fc9c490980534f1d1bc73fc4dd98d49
ThoKri10 <nintendodrex@hotmail.com>
2013-08-29 00:29:40 +0200

    Improved asteroids

--- Unity/Assets/SpawnAsteroids.js
+++ Unity/Assets/SpawnAsteroids.js
@@ -9,6 +9,8 @@ function Start ()
 		var asteroid_instance = Instantiate(asteroid, Random.insideUnitSphere * 1500, Random.rotation);
 		asteroid_instance.rigidbody.velocity = asteroid_instance.transform.forward * Random.Range(-15, 15);
 		asteroid_instance.transform.localScale *= Random.Range(0.005, 5);
+		asteroid_instance.rigidbody.mass = (1000/30) * asteroid_instance.transform.localScale.x;
+		asteroid_instance.rigidbody.AddTorque(asteroid_instance.transform.forward * 5000 * asteroid_instance.rigidbody.mass);
 	}
 }
 
